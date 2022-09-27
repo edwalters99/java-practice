0de3114eac18e065619a6e85033e774269c83f33
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-09-27 21:21:16 +1000

    Basic Input validation for mortgage calc

--- src/com/edjavapractice/mortgagecalc/Main.java
+++ src/com/edjavapractice/mortgagecalc/Main.java
@@ -7,17 +7,44 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        String principalInput = scanner.next();
-        System.out.print("Annual Interest Rate: ");
-        String interestInput = scanner.next();
-        System.out.print("Period (Years): ");
-        String yearsInput = scanner.next();
-        int principal = Integer.parseInt(principalInput);
-        float yearlyInterest = Float.parseFloat(interestInput);
-        int periods = Integer.parseInt(yearsInput) * MONTHS_IN_YEAR;
-        float monthlyInterestRate = yearlyInterest / PERCENT / MONTHS_IN_YEAR;
+
+        int principal;
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000) {
+                break;
+            }
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
+
+        Float yearlyInterest;
+        float monthlyInterestRate;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            yearlyInterest = scanner.nextFloat();
+            if (yearlyInterest > 0 && yearlyInterest <= 30) {
+                monthlyInterestRate = yearlyInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
+
+        int years;
+        int periods;
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextInt();
+            if (years >= 1 && years <= 30) {
+                periods = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
+
+
         double mortgagePayment = principal *
                 (
                         (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, periods)))
